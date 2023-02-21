604bf273b7e5cb19a4fca0ffbd313ab9969f2f6e
Viraat Chandra <viraat.chandra@hotmail.com>
2023-02-20 21:56:05 -0800

    test: get GSL, GLAD, GLFW, Vulkan loading

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 #include <GLFW/glfw3.h>
 #include <glm/vec3.hpp>
 
+#include "boost/thread.hpp"
+
 int main() {
   std::cout << glfwInit() << std::endl;
+  glm::vec3 v{1, 2, 3};
+  boost::thread t;
+  return 0;
 }
